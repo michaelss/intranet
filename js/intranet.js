38107d3ac3f9695c82a4dd322250ffeba2695827
Michael Schuenck dos Santos <michael@tre-to.jus.br>
2019-02-14 23:00:11 -0300

    Primeiro commit.

---
+++ js/intranet.js
@@ -0,0 +1,16 @@
+$(function() {
+    $('.item-menu').click(function() {
+      $('.item-menu').removeClass('is-active');
+      menu = $(this).data('menu');
+      if (sessionStorage['menuAtual'] == menu) {
+        $('#' + menu).toggle();
+        sessionStorage['menuAtual'] = '';
+      }
+      else {
+        $('.conteudo-menu').hide();
+        $('#' + menu).toggle();
+        sessionStorage['menuAtual'] = menu;
+        $(this).addClass('is-active');
+      }
+    });
+  });
